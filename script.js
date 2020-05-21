8522c5ee2c37f5a3ebb1309acff77ddf86c005b1
unknown <valdiviomc@outlook.com>
2020-05-21 14:24:24 -0300

    front game

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ snake [0] = {
     x: 8 * box,
     y: 8 * box
 }
-let direction = "rigth";
+let direction = "right";
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
-    y: Math.floor(Math.random() * 15 + 1) * box
+    y: Math.floor(Math.random() * 15 + 1) * box,
 }
 
 
@@ -39,10 +39,17 @@ function update (event){
 }
 
 function inicarJogo(){
-    if(snake[0].x > 15 + box && direction == "right") snake[0].x = 0;
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Gomer Over :)')
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -54,10 +61,15 @@ function inicarJogo(){
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
-    if (direction == "down") snakeY += box;
-    
-    snake.pop();
+    if(direction == "down") snakeY += box;
 
+    if(snakeX != food.x || snakeY != food.y){
+       snake.pop();
+    }
+    else{food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+    
     let newHead = {
         x: snakeX,
         y: snakeY,
